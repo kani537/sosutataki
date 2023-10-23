1dd908f2257ce759c748ab9de26c8998adedc95c
1step621 <onestep621@gmail.com>
2023-10-23 22:58:09 +0900

    コンボの追加 / フォントサイズ調整 / ストップウォッチがリセットされない不具合修正

--- main.cpp
+++ main.cpp
@@ -31,13 +31,12 @@ void nextPrimes(Array<int64> &primes, int diff) {
 
 void Main() {
   Window::SetStyle(WindowStyle::Sizable);
-  Font font(Scene::Size().x / 20, Typeface::Bold);
+  Font font(Scene::Size().x / 10, Typeface::Bold);
   Font diffFont(Scene::Size().x / 30);
   const Audio audioCorrect(U"./Quiz-Correct_Answer01-1.mp3");
   const Audio audioWrong(U"./Quiz-Wrong_Buzzer02-2.mp3");
   const Array<Audio> BGM = {Audio(U"./easy.m4a"), Audio(U"./easy.m4a"), Audio(U"./easy.m4a"), Audio(U"./hard.m4a")};
 
-  Stopwatch stopwatch{StartImmediately::Yes};
   const Array<StringView> diffs = {U"EASY", U"NORMAL", U"HARD", U"INSANE"},
                           results = {U"初心者", U"中級者", U"上級者", U"神"};
 
@@ -63,6 +62,8 @@ void Main() {
       if (flag)
         break;
     }
+    Stopwatch stopwatch{StartImmediately::Yes};
+    int combos = 0;
     score = 0;
     leftTime = 60;
     nextPrimes(primes, diff);
@@ -70,15 +71,20 @@ void Main() {
 
     // game
     while (System::Update()) {
-      if (Scene::Size().x / 20 != font.fontSize())
-        font = Font(Scene::Size().x / 20, Typeface::Bold);
+      if (Scene::Size().x / 10 != font.fontSize())
+        font = Font(Scene::Size().x / 10, Typeface::Bold);
 
       ClearPrint();
       Print << leftTime - stopwatch.s();
 
-      Rect(0, 0, ((double) (leftTime - stopwatch.sF()) / 60) * Scene::Size().x, 10).draw(leftTime - stopwatch.s() < 10 ? Palette::Red : Palette::Green);
-      for (size_t i = 1; i <= primes.size(); i++)
-        font(primes[i - 1]).draw(sin(stopwatch.sF() * 10) * 7 + 40, Arg::center(Scene::Size().x / (primes.size() + 1) * i, Scene::Size().y / 2));
+      double progress = (double) (leftTime - stopwatch.sF()) / 60;
+      Rect(0, 0, progress * Scene::Size().x, 10).draw(progress < 0.1 ? Palette::Red : Palette::Green);
+      for (size_t i = 1; i <= primes.size(); i++) {
+        int baseSize = Scene::Size().x / (diff * (primes.size() + 1) + 3);
+        double animeSize = sin(stopwatch.sF() * 10 + i * 10) * 7;
+        font(primes[i - 1]).draw(baseSize + animeSize, Arg::center(Scene::Size().x / (primes.size() + 1) * i, Scene::Size().y / 2));
+      }
+      font(combos).draw(Scene::Size().x / 20, Arg::center(Scene::Size().x / 2, Scene::Size().y / 4 * 3));
 
       for (size_t i = 0; i < primes.size(); i++)
         if (Input(InputDeviceType::Keyboard, 0x31 + i).down() && i < primes.size()) {
@@ -86,11 +92,13 @@ void Main() {
             audioCorrect.playOneShot();
             nextPrimes(primes, diff);
             leftTime += 3;
-            score++;
+            combos++;
+            score += combos;
             break;
           } else {
             audioWrong.playOneShot();
             leftTime -= 5;
+            combos = 0;
           }
         }
 
